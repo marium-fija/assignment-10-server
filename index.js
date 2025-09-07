fe3dde0a3d07ae0e9020044f8fa7101061e79746
Your Name <mariumfija@gmail.com>
2025-09-08 02:17:39 +0600

    all done backend comand

--- index.js
+++ index.js
@@ -28,6 +28,11 @@ async function run() {
 
     const gardenersCollection = client.db("gardenDB").collection("gardeners");
 
+    const usersCollection = client.db("gardenDB").collection("users");
+
+    const tipsCollection = client.db("gardenDB").collection("tips");
+
+
 
     const gardeners = [
       {
@@ -140,6 +145,195 @@ async function run() {
       res.send(gardener);
     });
 
+
+    // tips releted APIs
+
+    const tips = [
+  {
+    title: "How I Grow Tomatoes Indoors",
+    plantType: "Tomato",
+    difficulty: "Easy",
+    description: "I use pots and indoor lighting to grow juicy tomatoes at home.",
+    image: "https://i.ibb.co/tpcxTbbq/Tomato.jpg",
+    category: "Plant Care",
+    availability: "Public",
+    user: { email: "fija@example.com", name: "Fija" },
+    trending: true
+  },
+  {
+    title: "Indoor Basil Growth Tips",
+    plantType: "Basil",
+    difficulty: "Medium",
+    description: "Grow aromatic basil indoors with proper sunlight and watering.",
+    image: "https://i.ibb.co/1GtF1rFL/Basil.jpg",
+    category: "Herbs",
+    availability: "Public",
+    user: { email: "rahim@example.com", name: "Rahim" },
+    trending: true
+  },
+  {
+    title: "Vertical Strawberry Gardening",
+    plantType: "Strawberry",
+    difficulty: "Medium",
+    description: "Use vertical planters to save space and grow strawberries.",
+    image: "https://i.ibb.co/ynSvSZv1/Strawberry.jpg",
+    category: "Vertical Gardening",
+    availability: "Public",
+    user: { email: "salma@example.com", name: "Salma" },
+    trending: true
+  },
+  {
+    title: "Organic Carrot Farming",
+    plantType: "Carrot",
+    difficulty: "Hard",
+    description: "Tips to grow sweet carrots organically in your backyard.",
+    image: "https://i.ibb.co/rGMxJ8T3/Carrot.jpg",
+    category: "Composting",
+    availability: "Public",
+    user: { email: "jamal@example.com", name: "Jamal" },
+    trending: true
+  },
+  {
+    title: "Growing Roses in Pots",
+    plantType: "Rose",
+    difficulty: "Medium",
+    description: "Beautiful roses can bloom in small pots with proper care.",
+    image: "https://i.ibb.co/hFzGKWMx/Rose.jpg",
+    category: "Flower Care",
+    availability: "Public",
+    user: { email: "fatema@example.com", name: "Fatema" },
+    trending: true
+  },
+  {
+    title: "Quick Lettuce Harvest",
+    plantType: "Lettuce",
+    difficulty: "Easy",
+    description: "Learn to grow and harvest lettuce quickly in containers.",
+    image: "https://i.ibb.co/60BmqyXR/Lettuce.jpg",
+    category: "Plant Care",
+    availability: "Public",
+    user: { email: "kamal@example.com", name: "Kamal" },
+    trending: true
+  },
+  {
+    title: "Cucumber Trellis Tips",
+    plantType: "Cucumber",
+    difficulty: "Medium",
+    description: "Support your cucumber plants with trellis for better yield.",
+    image: "https://i.ibb.co/GQb5KVCP/cucumber.jpg",
+    category: "Vertical Gardening",
+    availability: "Public",
+    user: { email: "nadia@example.com", name: "Nadia" },
+    trending: false
+  },
+  {
+    title: "Sunflower Backyard Guide",
+    plantType: "Sunflower",
+    difficulty: "Easy",
+    description: "Grow tall sunflowers in your backyard easily.",
+    image: "https://i.ibb.co/BVwCGd0J/sunflower.jpg",
+    category: "Flower Care",
+    availability: "Public",
+    user: { email: "rafiq@example.com", name: "Rafiq" },
+    trending: false
+  }
+];
+
+const count2 = await tipsCollection.countDocuments();
+if (count2 === 0) {
+  const result = await tipsCollection.insertMany(tips);
+  console.log(`${result.insertedCount} tips inserted`);
+}
+
+app.get('/tips/trending', async (req, res) => {
+   const trendingTips = await tipsCollection.find({trending: true}).limit(6).toArray();
+  res.send(trendingTips);
+});
+
+app.get('/tips', async (req, res) => {
+  const result = await tipsCollection.find().toArray();
+  res.send(result);
+});
+
+app.get('/tips/:id', async (req, res) => {
+  const id = req.params.id;
+  const tip = await tipsCollection.findOne({_id: new ObjectId(id) });
+  res.send(tip);
+});
+
+app.post("/tips", async (req, res) =>{
+  const tip =req.body;
+  const result = await tipsCollection.insertOne(tip);
+  res.send(result);
+});
+
+
+// MY-TIPS releted APIs
+
+app.get('/tips', async (req, res) => {
+  const email = req.query.email;
+  let query = {};
+  if (email) {
+    query = {"user.email": email };
+  }
+  const result = await tipsCollection.find(query).toArray();
+  res.send(result);
+});
+
+app.get("/tips/:id", async (req, res) => {
+  const id =req.params.id;
+  const result =await tipsCollection.findOne({_id: new ObjectId(id) });
+  res.send(result);
+});
+
+app.delete("/tips/:id", async (req, res) => {
+  const id = req.params.id;
+  const result = await tipsCollection.deleteOne({_id: new ObjectId(id) });
+  res.send(result);
+});
+
+app.put("/tips/:id", async (req, res) => {
+  const id = req.params.id;
+  const updatedTip = req.body;
+  const result = await tipsCollection.updateOne(
+    {_id : new ObjectId(id) },
+    { $set : updatedTip}
+  );
+  res.send(result);
+});
+
+    // Users releted APIs
+
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.post('/users', async (req, res) => {
+      const userProfile = req.body;
+      const result = await usersCollection.insertOne(userProfile);
+      res.send(result);
+    })
+
+    app.patch('/users', async (req, res) => {
+      const {email, lastSignInTime} = req.body;
+      const filter = {email : email}
+      const updatedDoc = {
+        $set: {
+          lastSignInTime: lastSignInTime
+        }
+      }
+
+      const result =await usersCollection.updateOne(filter, updatedDoc)
+      res.send(result);
+    })
+
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)}
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    })
 
 
 
